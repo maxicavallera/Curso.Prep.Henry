53406e44f9abbbf1d9d6c71ce83c0bd4788db0b1
Maxi <maxi.cavallerar@gmail.com>
2020-09-30 01:28:35 -0300

    Homework

---
+++ 02-JS-I/ConceptosExplicados.c
@@ -0,0 +1,5 @@
+Strings: son conjuntos de caractéres o númerosuj78i6ºººººººº entre comillas dobles o simples. 
+Variables: es una palabra o símbolo al que le damos un valor para usar luego.
+Funciones: instruciones que le brindamos al sistema para que realice una acción, utilizando ''return'' devolvemos cierta información en particular, la cual podemos condicionar.
+Declaraciones: son guias y condiciones que le vamos asignando a la facunción para que devuelva cierta información.
+Valores booleanos: son valores que solo tienen dos opciones, 0 o 1, verdadero o falso, etc.
